6c3c3f02608cf473a954ba7a871f59afe6ef386d
Peter Bartha <peterbartha@pm.me>
2025-08-30 14:41:40 +0200

    WIP

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,6 @@
 use base64::DecodeError;
 use opaque_ke::errors::{InternalError, ProtocolError};
+use wasm_bindgen::prelude::*;
 
 pub enum Error {
     Protocol {
@@ -22,4 +23,21 @@ pub fn from_base64_error(context: &'static str) -> impl Fn(DecodeError) -> Error
 
 pub fn from_protocol_error(context: &'static str) -> impl Fn(ProtocolError) -> Error {
     move |error| Error::Protocol { context, error }
+}
+
+impl From<Error> for JsError {
+    fn from(err: Error) -> Self {
+        let msg = match err {
+            Error::Protocol { context, error } => {
+                format!("Opaque protocol error at \"{}\"; {}", context, error)
+            }
+            Error::Base64 { context, error } => {
+                format!("base64 decoding failed at \"{}\"; {}", context, error)
+            }
+            Error::Internal { context, error } => {
+                format!("Internal error at \"{}\"; {}", context, error)
+            }
+        };
+        JsError::new(&msg)
+    }
 }
